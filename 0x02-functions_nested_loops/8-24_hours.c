c66dff26bef75c3ce09011caef35cbe83cfcf154
Alabi Khalid <alabikhalid.12@gmail.com>
2022-09-15 16:14:55 -0700

    updated task 8(betty)

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,15 +11,14 @@ void jack_bauer(void)
 
 	for (h = 0; h <= 23; h++)
 	{
-		
-		for (m = 0; m <= 59; m++)
-		{
-			 _putchar((h / 10) + '0');
-			 _putchar((h % 10) + '0');
-			 _putchar(':');
-			 _putchar((m / 10) + '0');
-			 _putchar((m % 10) + '0');
-			 _putchar('\n');
-		}
+	for (m = 0; m <= 59; m++)
+	{
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+	}
 	}
 }
