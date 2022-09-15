3b5513186fe78a03b1d3173d1c56496e64feb908
Alabi Khalid <alabikhalid.12@gmail.com>
2022-09-15 05:25:43 -0700

    update task 2(betty)

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,14 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
-
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	while(count < 10)
+
+	while (count < 10)
 	{
 		int i;
+
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
