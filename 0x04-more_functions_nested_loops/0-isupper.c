d0ace31e1f4eb7a7abc80c209bdfd2c7ed43b653
Alabi Khalid <alabikhalid.12@gmail.com>
2022-09-17 16:16:46 -0700

    Task 9 isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
