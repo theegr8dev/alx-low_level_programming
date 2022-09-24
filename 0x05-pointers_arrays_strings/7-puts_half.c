7c3b3f37254a25fe561d37df3efae0ba1f9dec65
Alabi Khalid <alabikhalid.12@gmail.com>
2022-09-24 16:56:18 -0700

    Task 7 Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int odd, even;
+
+	while (*(str + len))
+	{
+		len++;
+	}
+
+	even = len / 2;
+	odd = (len - 1) / 2;
+	if (len % 2 == 0)
+	{
+		while (even < len)
+		{
+			_putchar(str[even]);
+			even++;
+		}
+	}
+	else
+	{
+		while (odd < len)
+		{
+			_putchar(str[odd]);
+			odd++;
+		}
+	}
+	_putchar(10);
+}
