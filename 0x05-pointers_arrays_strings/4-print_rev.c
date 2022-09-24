e07601fac027a0fe56303cb787ffb1d977ea9ae4
Alabi Khalid <alabikhalid.12@gmail.com>
2022-09-24 14:44:36 -0700

    updated task 4 (betty)

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@ void print_rev(char *s)
 	int len = 0;
 	int i = 0;
 
-	while ( *(s + len) != '\0')
+	while (*(s + len) != '\0')
 	{
 		len++;
 	}
 	while (i < len)
 	{
-		_putchar(s[len-i-1]);
+		_putchar(s[len - i - 1]);
 		i++;
 	}
 	_putchar(10);
